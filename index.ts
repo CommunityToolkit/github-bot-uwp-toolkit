28372b8d8853f5013f40c5e24ad12d9189357f09
Odonno <bottiau.david@laposte.net>
2017-09-09 15:46:40 +0200

    refactor: add type definition to issue node

--- index.ts
+++ index.ts
@@ -3,6 +3,51 @@ import * as https from 'https';
 
 import cron = require('node-cron');
 
+// Types definition
+type IssueNode = {
+    id: string;
+    author: {
+        login: string
+    };
+    createdAt: string;
+    comments: {
+        totalCount: number
+    };
+    lastComment: {
+        edges: {
+            node: {
+                updatedAt: string
+            }
+        }[];
+    };
+    lastTwoComments: {
+        edges: {
+            node: {
+                author: {
+                    login: string
+                },
+                body: string
+            }
+        }[];
+    };
+    commentAuthors: {
+        edges: {
+            node: {
+                author: {
+                    login: string
+                },
+            }
+        }[];
+    };
+    labels: {
+        edges: {
+            node: {
+                name: string
+            }
+        }[];
+    };
+}
+
 // GitHub Api calls
 
 const closeGitHubIssue = (headers: any, issueId: string) => {
@@ -39,16 +84,16 @@ const commentGitHubIssueMutation = (issueId: string, comment: string): string =>
       }`;
 }
 
-const getAllGitHubIssuesRecursively = (headers: any, repoOwner: string, repoName: string, afterCursor: string, callback: (issues: any[]) => any) => {
+const getAllGitHubIssuesRecursively = (headers: any, repoOwner: string, repoName: string, afterCursor: string, callback: (issues: IssueNode[]) => any) => {
     performGitHubGraphqlRequest(headers, {
         query: getGitHubIssuesQuery(repoOwner, repoName, afterCursor)
     }, (response) => {
         if (response.data.repository.issues.pageInfo.hasNextPage) {
             getAllGitHubIssuesRecursively(headers, repoOwner, repoName, response.data.repository.issues.pageInfo.endCursor, (issues) => {
-                callback(issues.concat(response.data.repository.issues.edges));
+                callback(issues.concat(response.data.repository.issues.edges.map(edge => edge.node)));
             });
         } else {
-            callback(response.data.repository.issues.edges);
+            callback(response.data.repository.issues.edges.map(edge => edge.node));
         }
     });
 }
@@ -74,9 +119,9 @@ const getGitHubIssuesQuery = (repoOwner: string, repoName: string, afterCursor?:
                 },
                 lastComment: comments(last: 1) {
                     edges {
-                    node {
-                      updatedAt
-                    }
+                      node {
+                        updatedAt
+                      }
                   }
                 },
                 lastTwoComments: comments(last: 2) {
@@ -183,14 +228,14 @@ const addDays = (date: Date, days: number): Date => {
 // TODO : send a message with links to unclosed issues
 
 // issues - should detect if there was no response from the community after a period of time
-const detectIfNoResponseFromCommunity = (githubApiHeaders: any, issue: any, exclusiveLabels: string[]) => {
+const detectIfNoResponseFromCommunity = (githubApiHeaders: any, issue: IssueNode, exclusiveLabels: string[]) => {
     // check if there is only one user who write a message (the creator of the message)
-    const loginsOfAuthors: string[] = distinct(issue.node.commentAuthors.edges.map(edge => edge.node.author.login));
-    const issueHasNoResponse = distinct(loginsOfAuthors.filter(c => c !== issue.node.author.login)).length === 0;
+    const loginsOfAuthors: string[] = distinct(issue.commentAuthors.edges.map(edge => edge.node.author.login));
+    const issueHasNoResponse = distinct(loginsOfAuthors.filter(c => c !== issue.author.login)).length === 0;
 
     if (issueHasNoResponse) {
         // check if the issue contains an exclusive labels (only issues without those labels are usable)
-        const containsExclusiveLabels = issue.node.labels.edges.map(edge => edge.node).filter(label => {
+        const containsExclusiveLabels = issue.labels.edges.map(edge => edge.node).filter(label => {
             return exclusiveLabels.some(l => l === label.name);
         }).length > 0;
 
@@ -198,46 +243,46 @@ const detectIfNoResponseFromCommunity = (githubApiHeaders: any, issue: any, excl
             // check if the issue was created 7 days ago
             const today = new Date();
 
-            if (new Date(issue.node.createdAt) < addDays(today, -7)) {
+            if (new Date(issue.createdAt) < addDays(today, -7)) {
                 // send a message with a ping to the team
-                commentGitHubIssue(githubApiHeaders, issue.node.id, `No response from the community. ping @nmetulev`);
+                commentGitHubIssue(githubApiHeaders, issue.id, `No response from the community. ping @nmetulev`);
             }
         }
     }
 }
 
 // issues - should send a reminder after a period of time, up to 2 successive alerts
-const sendReminderAfterAPeriodOfTime = (githubApiHeaders: any, botUsername: string, issue: any, exclusiveLabels: string[]) => {
+const sendReminderAfterAPeriodOfTime = (githubApiHeaders: any, botUsername: string, issue: IssueNode, exclusiveLabels: string[]) => {
     // check if at least two users write a message (one user other than the author)
-    const loginsOfAuthors: string[] = distinct(issue.node.commentAuthors.edges.map(edge => edge.node.author.login));
-    const issueHasResponse = distinct(loginsOfAuthors.filter(c => c !== issue.node.author.login)).length > 0;
+    const loginsOfAuthors: string[] = distinct(issue.commentAuthors.edges.map(edge => edge.node.author.login));
+    const issueHasResponse = distinct(loginsOfAuthors.filter(c => c !== issue.author.login)).length > 0;
 
     if (issueHasResponse) {
         // check if the issue contains an exclusive labels (only issues without those labels are usable)
-        const containsExclusiveLabels = issue.node.labels.edges.map(edge => edge.node).filter(label => {
+        const containsExclusiveLabels = issue.labels.edges.map(edge => edge.node).filter(label => {
             return exclusiveLabels.some(l => l === label.name);
         }).length > 0;
 
         if (!containsExclusiveLabels) {
             // check if last message was sent 7 days ago
-            const lastComment = issue.node.lastComment.edges[0];
+            const lastComment = issue.lastComment.edges[0];
             const today = new Date();
 
             if (lastComment && new Date(lastComment.node.updatedAt) < addDays(today, -7)) {
                 // less than 3 messages or
                 // check if last messages of the issue contains less than 2 successive messages of the bot
-                const lastTwoMessages = issue.node.lastTwoComments.edges.map(edge => edge.node);
+                const lastTwoMessages = issue.lastTwoComments.edges.map(edge => edge.node);
                 const reminderMessagesFromBot = lastTwoMessages.filter(c => {
                     return (c.author.login === botUsername && c.body.indexOf('This issue seems inactive') > -1);
                 });
 
-                if (issue.node.comments.totalCount < 3 || reminderMessagesFromBot.length < 2) {
+                if (issue.comments.totalCount < 3 || reminderMessagesFromBot.length < 2) {
                     // send a message to the creator that issue will be close in X days
                     const daysBeforeClosingIssue = 7 * (2 - reminderMessagesFromBot.length);
 
                     commentGitHubIssue(
                         githubApiHeaders,
-                        issue.node.id,
+                        issue.id,
                         `This issue seems inactive. It will automatically be closed in ${daysBeforeClosingIssue} days if there is no activity.`);
                 }
             }
@@ -246,37 +291,37 @@ const sendReminderAfterAPeriodOfTime = (githubApiHeaders: any, botUsername: stri
 }
 
 // issues - should close inactive issue after 3 successive alerts
-const closeInactiveIssueAfterThreeSuccessiveAlerts = (githubApiHeaders: any, botUsername: string, issue: any, exclusiveLabels: string[]) => {
+const closeInactiveIssueAfterThreeSuccessiveAlerts = (githubApiHeaders: any, botUsername: string, issue: IssueNode, exclusiveLabels: string[]) => {
     // check if at least two users write a message (one user other than the author)
-    const loginsOfAuthors: string[] = distinct(issue.node.commentAuthors.edges.map(edge => edge.node.author.login));
-    const issueHasResponse = distinct(loginsOfAuthors.filter(c => c !== issue.node.author.login)).length > 0;
+    const loginsOfAuthors: string[] = distinct(issue.commentAuthors.edges.map(edge => edge.node.author.login));
+    const issueHasResponse = distinct(loginsOfAuthors.filter(c => c !== issue.author.login)).length > 0;
 
     if (issueHasResponse) {
         // check if the issue contains an exclusive labels (only issues without those labels are usable)
-        const containsExclusiveLabels = issue.node.labels.edges.map(edge => edge.node).filter(label => {
+        const containsExclusiveLabels = issue.labels.edges.map(edge => edge.node).filter(label => {
             return exclusiveLabels.some(l => l === label.name);
         }).length > 0;
 
         if (!containsExclusiveLabels) {
             // check if last message was sent 7 days ago
-            const lastComment = issue.node.lastComment.edges[0];
+            const lastComment = issue.lastComment.edges[0];
             const today = new Date();
 
-            if (lastComment && new Date(lastComment.updatedAt) < addDays(today, -7)) {
+            if (lastComment && new Date(lastComment.node.updatedAt) < addDays(today, -7)) {
                 // check if last messages of the issue contains exactly 2 successive messages of the bot
-                const lastTwoMessages = issue.node.lastTwoComments.edges.map(edge => edge.node);
+                const lastTwoMessages = issue.lastTwoComments.edges.map(edge => edge.node);
                 const reminderMessagesFromBot = lastTwoMessages.filter(c => {
-                    return (c.user.login === botUsername && c.body.indexOf('This issue seems inactive') > -1);
+                    return (c.author.login === botUsername && c.body.indexOf('This issue seems inactive') > -1);
                 });
 
                 if (reminderMessagesFromBot.length === 2) {
                     // close issue and send a message that issue got no answer from the creator
                     commentGitHubIssue(
                         githubApiHeaders,
-                        issue.node.id,
+                        issue.id,
                         'Issue is inactive. It was automatically closed.');
 
-                    closeGitHubIssue(githubApiHeaders, issue.node.id);
+                    closeGitHubIssue(githubApiHeaders, issue.id);
                 }
             }
         }
